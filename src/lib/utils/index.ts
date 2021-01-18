1cfea2ac24b4bd9579cc2176c9a3a929be7d4108
legitatx <legitmodern.dev@gmail.com>
2021-01-18 09:41:14 -0500

    Start work on user profile

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,5 +1,14 @@
 import { message, notification } from "antd";
 
+export const iconColor = "#1890ff";
+
+export const formatListingPrice = (price: number, round = true) => {
+  const formatttedLisittingPrice = round
+    ? Math.round(price / 100)
+    : price / 100;
+  return `${formatttedLisittingPrice}`;
+};
+
 export const displaySuccessNotification = (
   message: string,
   description?: string
