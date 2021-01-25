f476986ba3a460db623a4e4e0f3231e3c85719f3
legitatx <legitmodern.dev@gmail.com>
2021-01-25 12:16:37 -0500

    Search bar at top of listings

--- src/sections/AppHeader/index.tsx
+++ src/sections/AppHeader/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Layout } from "antd";
+import React, { useState, useEffect } from "react";
+import { Link, withRouter, RouteComponentProps } from "react-router-dom";
+import { Input, Layout } from "antd";
 import { Viewer } from "../../lib/types";
+import { displayErrorMessage } from "../../lib/utils";
 import { MenuItems } from "./components";
 
 import logo from "./assets/tinyhouse-logo.png";
@@ -12,20 +13,58 @@ interface Props {
 }
 
 const { Header } = Layout;
+const { Search } = Input;
 
-export const AppHeader = ({ viewer, setViewer }: Props) => {
-  return (
-    <Header className="app-header">
-      <div className="app-header__logo-search-section">
-        <div className="app-header__logo">
-          <Link to="/">
-            <img src={logo} alt="App logo" />
-          </Link>
+export const AppHeader = withRouter(
+  ({ viewer, setViewer, location, history }: Props & RouteComponentProps) => {
+    const [search, setSearch] = useState("");
+
+    useEffect(() => {
+      const { pathname } = location;
+      const splitPathname = pathname.split("/");
+
+      if (!pathname.includes("/listings")) {
+        setSearch("");
+        return;
+      }
+
+      if (pathname.includes("/listings") && splitPathname.length === 3) {
+        setSearch(splitPathname[2]);
+      }
+    }, [location]);
+
+    const onSearch = (value: string) => {
+      const trimmedValue = value.trim();
+
+      if (trimmedValue) {
+        history.push(`/listings/${trimmedValue}`);
+      } else {
+        displayErrorMessage("Please enter a valid search term!");
+      }
+    };
+
+    return (
+      <Header className="app-header">
+        <div className="app-header__logo-search-section">
+          <div className="app-header__logo">
+            <Link to="/">
+              <img src={logo} alt="App logo" />
+            </Link>
+          </div>
+          <div className="app-header__search-input">
+            <Search
+              placeholder="Search 'San Francisco'"
+              enterButton
+              value={search}
+              onChange={(event) => setSearch(event.target.value)}
+              onSearch={onSearch}
+            />
+          </div>
         </div>
-      </div>
-      <div className="app-header__menu-section">
-        <MenuItems viewer={viewer} setViewer={setViewer} />
-      </div>
-    </Header>
-  );
-};
+        <div className="app-header__menu-section">
+          <MenuItems viewer={viewer} setViewer={setViewer} />
+        </div>
+      </Header>
+    );
+  }
+);
